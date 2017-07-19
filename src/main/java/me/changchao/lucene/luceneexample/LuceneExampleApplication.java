f697f0cb7f2dc5c2b20dc144f994d333c81fda95
chang chao <chang-chao@hotmail.com>
2017-07-19 17:01:57 +0900

    initial import

---
+++ src/main/java/me/changchao/lucene/luceneexample/LuceneExampleApplication.java
@@ -0,0 +1,74 @@
+package me.changchao.lucene.luceneexample;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.IntPoint;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.FSDirectory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.extern.apachecommons.CommonsLog;
+
+@SpringBootApplication
+@CommonsLog
+public class LuceneExampleApplication implements CommandLineRunner {
+	public static final int MAX_RESULTS = 10;
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuceneExampleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String destination = "index";
+		String intFieldName = "intField";
+
+		int docCount = 1000;
+
+		File dstFile = new File(destination);
+		if (dstFile.exists()) {
+			FileUtils.forceDelete(dstFile);
+		}
+
+		StandardAnalyzer analyzer = new StandardAnalyzer();
+		IndexWriterConfig config = new IndexWriterConfig(analyzer);
+		config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+		FSDirectory directory = FSDirectory.open(Paths.get(destination));
+		try (final IndexWriter writer = new IndexWriter(directory, config);) {
+
+			for (int i = 0; i < docCount; i++) {
+				Document doc = new Document();
+				doc.add(new IntPoint(intFieldName, i));
+				writer.addDocument(doc);
+			}
+		}
+
+		IndexReader reader = DirectoryReader.open(directory);
+		final IndexSearcher searcher = new IndexSearcher(reader);
+
+		// QueryParser parser = new QueryParser("id", analyzer);
+		// String searchString = "1";
+		// Query query = parser.parse(searchString);
+
+		Query query = IntPoint.newSetQuery(intFieldName, 1, 2, 3, 4, 5);
+		TopDocs result = searcher.search(query, MAX_RESULTS);
+		ScoreDoc[] docs = result.scoreDocs;
+		for (ScoreDoc scoreDoc : docs) {
+			log.debug("scoreDoc:" + scoreDoc);
+		}
+
+	}
+}
