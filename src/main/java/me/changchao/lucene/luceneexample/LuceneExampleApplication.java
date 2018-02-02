cf4e8d8d22262fffe31654dd6789ba497d2134e7
chang chao <chang-chao@hotmail.com>
2018-02-02 15:42:35 +0900

    upgrade to lucene 7.2.1

--- src/main/java/me/changchao/lucene/luceneexample/LuceneExampleApplication.java
+++ src/main/java/me/changchao/lucene/luceneexample/LuceneExampleApplication.java
@@ -6,14 +6,13 @@ import java.nio.file.Paths;
 import org.apache.commons.io.FileUtils;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field.Store;
-import org.apache.lucene.document.LegacyIntField;
+import org.apache.lucene.document.IntPoint;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.LegacyNumericRangeQuery;
+import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
@@ -50,9 +49,9 @@ public class LuceneExampleApplication implements CommandLineRunner {
 		FSDirectory directory = FSDirectory.open(Paths.get(destination));
 		try (final IndexWriter writer = new IndexWriter(directory, config);) {
 
-			for (int i = 0; i < docCount; i++) {
+			for (int i = 0; i < docCount; i += 2) {
 				Document doc = new Document();
-				doc.add(new LegacyIntField(intFieldName, i, Store.YES));
+				doc.add(new IntPoint(intFieldName, i));
 				writer.addDocument(doc);
 			}
 		}
@@ -65,7 +64,7 @@ public class LuceneExampleApplication implements CommandLineRunner {
 		// Query query = parser.parse(searchString);
 
 		// Query query = IntPoint.newSetQuery(intFieldName, 1, 2, 3, 4, 5);
-		LegacyNumericRangeQuery<Integer> query = LegacyNumericRangeQuery.newIntRange(intFieldName, 200, 500, true, false);
+		Query query = IntPoint.newRangeQuery(intFieldName, 1, 800);
 		TopDocs result = searcher.search(query, MAX_RESULTS);
 		ScoreDoc[] docs = result.scoreDocs;
 		for (ScoreDoc scoreDoc : docs) {
